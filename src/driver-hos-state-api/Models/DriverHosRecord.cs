d910a2baded1042a9494847fb0d0c38cf99dc5b2
Sreenivas Upputholla <supputholl@usxpress.com>
2025-07-19 23:12:37 +0530

    master set up

---
+++ src/driver-hos-state-api/Models/DriverHosRecord.cs
@@ -0,0 +1,9 @@
+﻿namespace driver_hos_state_api.Models
+{
+    public class DriverHosRecord
+    {
+        public string State { get; set; }
+        public int OnDutyHours { get; set; }
+        public int DrivingHours { get; set; }
+    }
+}
---
+++ src/driver-hos-state-api/Program.cs
@@ -0,0 +1,15 @@
+namespace driver_hos_state_api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
